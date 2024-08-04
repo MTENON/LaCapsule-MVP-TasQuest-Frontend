2249886b85efb66315614dd04998f1ce5d3e4a99
Maxime Tenon <maxime.tenon@gmail.com>
2024-08-04 10:48:34 +0200

    added reducer structure

---
+++ reducers/users.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+//Valeurs initiales
+const initialState = {
+    username: '',
+    token: '',
+};
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        updateUsername: (state, action) => {
+            state.username = action.payload;
+        },
+        updateToken: (state, action) => {
+            state.token = action.payload;
+        }
+    }
+})
+
+export const { updateUsername, updateToken } = userSlice.actions;
+export default userSlice.reducer;
