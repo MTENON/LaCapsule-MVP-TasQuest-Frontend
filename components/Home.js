e4e2528def6e6da78fcd678b5938c0e8dab61621
Maxime Tenon <maxime.tenon@gmail.com>
2024-08-05 11:45:46 +0200

    healthbar done

--- components/Home.js
+++ components/Home.js
@@ -1,4 +1,5 @@
 import styles from '../styles/Home.module.css';
+import Healthbar from './atoms/healthbar';
 
 function Home() {
   return (
@@ -7,6 +8,7 @@ function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <Healthbar health={65}></Healthbar>
       </main>
     </div>
   );
