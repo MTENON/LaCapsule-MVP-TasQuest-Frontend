50b7325a5090d2e9c008a1707d27e0c12cd17ac0
Angelique <angelique.parizet@gmail.com>
2024-08-06 13:01:34 +0200

    create questiondescription page

---
+++ pages/questdescription.js
@@ -0,0 +1,11 @@
+import Layout from "../components/layouts/Layout";
+
+function QuestDecritpionPage() {
+    return (
+        <Layout>
+            <h1>Page Inventaire</h1>
+        </Layout>
+    );
+}
+
+export default QuestDecritpionPage;
