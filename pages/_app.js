08f88b233ed92ab4e3672d5482775bdc6296049d
Maxime Tenon <maxime.tenon@gmail.com>
2024-08-07 16:49:58 +0200

    persistance of store on

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,13 @@ import Head from "next/head";
 
 // --- Importation redux --- //
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import user from "../reducers/users";
+
+// --- Importation persistance redux --- //
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
 
 // --- Importation mui/material --- //
 import { ThemeProvider } from "@mui/material";
@@ -12,23 +17,34 @@ import theme from "../theme/index";
 import SideMenu from "../components/molecules/SideMenu";
 
 // --- Store configuration --- //
+
+const reducers = combineReducers({ user });
+
+const persistConfig = { key: 'tasquest', storage };
+
 const store = configureStore({
-    reducer: { user },
+    reducer: persistReducer(persistConfig, reducers),
+
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false })
 });
+
+const persistor = persistStore(store);
 
 function App({ Component, pageProps }) {
     return (
         <>
             <ThemeProvider theme={theme}>
                 <Provider store={store}>
-                    <Head>
-                        <title>TasQuest</title>
-                        <style>
-                            @import
-                            url('https://fonts.googleapis.com/css2?family=Fondamento:ital@0;1&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
-                        </style>
-                    </Head>
-                    <Component {...pageProps} />
+                    <PersistGate persistor={persistor}>
+                        <Head>
+                            <title>TasQuest</title>
+                            <style>
+                                @import
+                                url('https://fonts.googleapis.com/css2?family=Fondamento:ital@0;1&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+                            </style>
+                        </Head>
+                        <Component {...pageProps} />
+                    </PersistGate>
                 </Provider>
             </ThemeProvider>
         </>
