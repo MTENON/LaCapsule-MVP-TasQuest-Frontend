cca9e918b9f1add1403ffdd012604a4c4f20f8e5
Maryam <ma.abde.contact@gmail.com>
2024-08-10 15:54:35 +0200

    title atom css changes

--- components/atoms/TitleAtoms.js
+++ components/atoms/TitleAtoms.js
@@ -2,18 +2,21 @@ import styles from "../../styles/atoms/TitleAtom.module.css";
 import Button from "./Button";
 
 const TitleAtoms = ({ title, func }) => {
-    return (
-        <>
-            <div className={styles.container}>
-                <h1>{title}</h1>
-                <Button
-                    icon="material-symbols:tag"
-                    variant="primary"
-                    func={func}
-                ></Button>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.btn}></div>
+        <h1 className={styles.text}>{title}</h1>
+        <div className={styles.btn}>
+          <Button
+            icon="material-symbols:tag"
+            variant="primary"
+            func={func}
+          ></Button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default TitleAtoms;
