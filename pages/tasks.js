ef6da0dc64da203afba35dba8ed80f2a50f8e1c1
Angelique <angelique.parizet@gmail.com>
2024-08-14 21:34:19 +0200

    token verification and login redirection added

--- pages/tasks.js
+++ pages/tasks.js
@@ -4,8 +4,31 @@ import Layout from "../components/layouts/Layout";
 import Tasks from "../components/organisms/Tasks";
 import Todos from "../components/organisms/Todos";
 import styles from "../styles/pages/tasks.module.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
 
 function TasksPage() {
+    const test = useSelector((state) => state.user.token);
+
+    const router = useRouter();
+    useEffect(() => {
+        try {
+            const storedData = localStorage.getItem("persist:tokenify");
+            const parsedData = JSON.parse(JSON.parse(storedData).user).value;
+            const token = parsedData.token;
+            console.log("parsedStoredData", token);
+
+            if (token) {
+                console.log(token);
+            } else {
+                router.push("/");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }, [router]);
+
     const [selectedTask, setSelectedTask] = useState(null);
     const [forceUpdate, setForceUpdate] = useState(false);
 
